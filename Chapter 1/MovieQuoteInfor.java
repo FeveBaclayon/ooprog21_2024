aa45133a4eacd1e880570c8f56f6d59a851ca5eb
Baclayon27 <lanzraijin@gmail.com>
2024-08-14 22:10:42 +0800

    Create MovieQuoteInfor.java

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfor {
+   public static void main(String args[]) {
+      System.out.println("theres no place like home");
+      System.out.println("The Wizard of Oz ");
+      System.out.println("1939");
+       System.out.println("starring Judy Garland and Frank Morgan");
+   }
+}
